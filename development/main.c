9be36478abb9511505b025398f128f654a486679
vikrantwaje <viwa0175@colorado.edu>
2019-03-17 11:33:01 -0600

    Created timer for handling heartbeat and logger

--- development/main.c
+++ development/main.c
@@ -1,3 +1,6 @@
+/********************************************************************************************
+*					FILENAME: main.c
+**********************************************************************************************/
 /*
  * Author: Tanmay Chaturvedi, Vikrant Waje
  * Date Created: March 16, 2019
@@ -9,7 +12,9 @@
  * 
  * 
  * */
-
+/****************************************************************************************
+*					HEADER FILE SECTION
+*****************************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -27,146 +32,243 @@
 #include <netinet/in.h>
 #include <sys/time.h>
 #include <netdb.h>
-#include "main.h"
+#include<stdbool.h>
+//#include "main.h"
 
 
+/****************************************************************************************
+*				GLOBAL VARIABLE SECTION
+*****************************************************************************************/
+// Logger timer id
+timer_t log_timer_id;
+//Heartbeat timer id
+timer_t heartbeat_timer_id;
+//timespec struct for giving interval values
+struct itimerspec timer_setting;
+//Flag to be set in log handler
+int log_timer_flag;
+//Flag to be set in heartbeat handler
+int heartbeat_timer_flag;
 
+struct sigevent signal_specification;
 
-int main()
+/***********************************************************************************************
+ * @brief logger  timer handler
+ *
+ * Handler that executes periodically to generate logging interval
+ * @param null
+ *
+ * @return null
+ *********************************************************************************************/void log_timer_handler(int num){
+
+	log_timer_flag = 1;
+	printf("\n\rHi logger");
+}
+/***********************************************************************************************
+ * @brief heartbeat timer handler
+ *
+ * Handler that executes periodically to generate heartbeat interval
+ * @param null
+ *
+ * @return null
+ *********************************************************************************************/
+void heartbeat_timer_handler(int num){
+
+	heartbeat_timer_flag = 1;
+	printf("\n\rHi heartbeat");
+}
+/***********************************************************************************************
+ * @brief Create log timer
+ *
+ * Responsible for creating log timer which expires periodically to collect sensor information an
+ * nd socket status that is logged into log file
+ * @param null
+ *
+ * @return bool: Indicates whether intiialisation was successfull or not
+ *********************************************************************************************/
+bool create_log_timer(){
+	signal_specification.sigev_notify = SIGEV_THREAD;
+	signal_specification.sigev_notify_function = &log_timer_handler;
+	signal_specification.sigev_value.sival_ptr = "State collection timer";
+	timer_create(CLOCK_REALTIME,&signal_specification,&log_timer_id);
+	timer_setting.it_value.tv_sec =1;
+	timer_setting.it_value.tv_nsec = 0;
+	timer_setting.it_interval.tv_sec = 0;
+	timer_setting.it_interval.tv_nsec = 100000000;
+	timer_settime(log_timer_id,0,&timer_setting,NULL);
+	return 0;
+}
+
+/***********************************************************************************************
+ * @brief Create heartbeat timer
+ *
+ * Responsible for generating heartbeat for each of the four thread
+ * @param null
+ *
+ * @return bool: Indicates whether intiialisation was successfull or not
+ *********************************************************************************************/
+bool create_heartbeat_timer(){
+
+	signal_specification.sigev_notify = SIGEV_THREAD;
+	signal_specification.sigev_notify_function = &heartbeat_timer_handler;
+	signal_specification.sigev_value.sival_ptr = "Heartbeat timer";
+	timer_create(CLOCK_REALTIME,&signal_specification,&heartbeat_timer_id);
+	timer_setting.it_value.tv_sec =1;
+	timer_setting.it_value.tv_nsec = 0;
+	timer_setting.it_interval.tv_sec = 0;
+	timer_setting.it_interval.tv_nsec = 50000000;
+	timer_settime(heartbeat_timer_id,0,&timer_setting,NULL);
+	return 0;
+}
+
+/***********************************************************************************************
+ * @brief light sensor thread
+ *
+ * Get the LUX values from light sensor attached using I2C interface and report it periodically t * o logger thread
+ *
+ * @param null
+ *
+ * @return null
+ *********************************************************************************************/
+void *light_sensor_thread(){
+
+	printf("\n\rlight sensor thread");
+	while(1);
+}
+
+/***********************************************************************************************
+ * @brief logger thread
+ *
+ * Log the values of temperature, light sensor and socket status in log file created by logger task 
+ * into file opened by logger thread
+ *
+ * @param null
+ *
+ * @return null
+ *********************************************************************************************/
+void *logger_thread(){
+
+	printf("\n\rLogger thread");
+	while(1);
+}
+/***********************************************************************************************
+ * @brief Temperature thread
+ *
+ * Get the temperature value periodically from temperature sensor and logs 
+ * into file opened by logger thread
+ *
+ * @param null
+ *
+ * @return null
+ *********************************************************************************************/
+
+void *temperature_thread(){
+
+	printf("\n\rTemperature thread");
+	while(1);
+
+}
+/*******************************************************************************************
+ * @brief Socket thread
+ *
+ * Waits for a connection initiated by an external client.
+ * Once connected, accepts commands from client and acts accordingly.
+ * If clients commands the connection to "close", server thread disconnects,
+ * closes the socket connection and waits for a new connection.
+ *
+ * @param null
+ *
+ * @return null
+ ********************************************************************************************/
+void *socket_thread()
+{
+	printf("\n\rServer thread");
+}
+
+/*******************************************************************************************
+ * @brief Main function
+ *
+ * Responsible for creating four threads for execution namely light sensor thread, temperature th * read, logger thread and socket thread
+ *
+ * @param null
+ *
+ * @return null
+ ********************************************************************************************/
+ int main()
 {
 	pthread_t thread1, thread2, thread3, thread4;
 
-	int ret_status1, ret_status2, ret_status3, ret_status4;
+	int ret_status;
+
+	/*Initialising timer for logger task*/
+	ret_status =create_log_timer();
+	if(ret_status !=0){
+	fprintf( stderr, "log timer not created , Error Code: %d\n", ret_status);
+	}
+
+	/*Initialise timer for heartbeat task*/
+	ret_status =create_heartbeat_timer();
+	if(ret_status !=0){
+	fprintf( stderr, "heartbeat timer not created , Error Code: %d\n", ret_status);
+	}
+
+	/* Socket thread creation*/
+	ret_status = pthread_create( &thread1, NULL, socket_thread,0);
+	if( ret_status )
+	{
+		fprintf( stderr, "socket_thread not created, Error Code: %d\n", ret_status);
+		return 0;
+	}
 	
-	ret_status1 = pthread_create( &thread1, NULL, socket_thread, /*(void*)&fileinstance*/);
-	ret_status1 = pthread_create( &thread2, NULL, temperature_thread, /*(void*)&fileinstance*/);
-	ret_status1 = pthread_create( &thread3, NULL, light_sensor_thread, /*(void*)&fileinstance*/);
-	ret_status1 = pthread_create( &thread4, NULL, logger_thread, /*(void*)&fileinstance*/);
-
-	if( ret_status1 )
+	/*Temperature thread creation*/
+	ret_status = pthread_create( &thread2, NULL, temperature_thread,0);
+	if( ret_status )
 	{
-		fprintf( stderr, "socket_thread not created, Error Code: %d\n", ret_status1);
-		return 0;
-	}
-	if( ret_status2 )
-	{
-		fprintf( stderr, "temperature_thread not created, Error Code: %d\n", ret_status2);
-		return 0;
-	}
-	if( ret_status3 )
-	{
-		fprintf( stderr, "light_sensor_thread not created, Error Code: %d\n", ret_status3);
-		return 0;
-	}
-	if( ret_status4 )
-	{
-		fprintf( stderr, "logger_thread not created, Error Code: %d\n", ret_status4);
+		fprintf( stderr, "temperature_thread not created, Error Code: %d\n", ret_status);
 		return 0;
 	}
 
-	pthread_join( thread1, NULL);
-	pthread_join( thread2, NULL);
-	pthread_join( thread3, NULL);
-	pthread_join( thread4, NULL);
-
-	printf("Main Thread Exited Successfully \n");
+	/*light sensor thread creation*/
+	ret_status = pthread_create( &thread3, NULL, light_sensor_thread,0);
+	if( ret_status )
+	{
+		fprintf( stderr, "light_sensor_thread not created, Error Code: %d\n", ret_status);
+		return 0;
+	}
 	
+	/*logger thread creation*/
+	ret_status = pthread_create( &thread4, NULL, logger_thread,0);
+
+	if( ret_status )
+	{
+		fprintf( stderr, "logger_thread not created, Error Code: %d\n", ret_status);
+		return 0;
+	}
+
+	/*Socket thread join*/
+	ret_status = pthread_join( thread1, NULL);
+	if(ret_status){
+		fprintf( stderr, "socket thread not joined successfully, Error Code: %d\n", ret_status);
+	}
+	/*Temperature thread join*/
+	ret_status =	pthread_join( thread2, NULL);
+	if(ret_status){
+		fprintf( stderr, "temperature_thread not joined successfully, Error Code: %d\n", ret_status);
+	}
+
+	/*Light sensor thread join*/
+	ret_status =	pthread_join( thread3, NULL);
+	if(ret_status){
+		fprintf( stderr, "light_sensor thread not joined successfully, Error Code: %d\n", ret_status);
+	}
+
+	/*Logger thread join*/
+	ret_status =	pthread_join( thread4, NULL);
+	if(ret_status){
+		fprintf( stderr, "logger thread not joined successfully, Error Code: %d\n", ret_status);
+	}	printf("Main Thread Exited Successfully \n");
+
 	return 1;
 }
 
 
-/**
-* @brief Socket thread
-*
-* Waits for a connection initiated by an external client.
-* Once connected, accepts commands from client and acts accordingly.
-* If clients commands the connection to "close", server thread disconnects,
-* closes the socket connection and waits for a new connection.
-*
-* @param null
-*
-* @return null
-*/
-void *socket_thread(void)
-{
-	/*Server Thread*/
-	int recv_stat;
-	char str[20];
-	int send_data_len;
-	struct sockaddr_in server_addr;
-
-	int socket_flag = 0;	/*Not Connected*/
-
-	while( socket_flag == 0 )
-	{
-		socket_fd = socket(AF_INET, SOCK_STREAM,0);
-
-		if(socket_fd < 0)
-		{
-			perror("Server: socket()");
-			exit(1);
-		}
-
-		server_addr.sin_family = AF_INET;
-		server_addr.sin_addr.s_addr = INADDR_ANY;
-		server_addr.sin_port = htons(PORT_NUM);
-		socket_fd = socket(AF_INET, SOCK_STREAM,0);
-
-		if(bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)))
-		{
-			perror("Server: bind()");
-			exit(1);
-		}
-
-		listen(socket_fd, 5);
-
-		sock_stat = accept(socket_fd, (struct sockaddr *) 0,0);
-
-		if( sock_stat == -1)
-		{
-				perror("Server: accept()");
-	//			socket_flag = 0;
-		}
-
-		socket_flag = 1;
-
-		while(socket_flag == 1)
-		{
-			memset(str,0,sizeof(str));
-			if((recv_stat = recv(sock_stat, str,sizeof(str),0) < 0))
-			{
-				perror("read error");
-			}
-			RxBuf[0] = str;
-
-			if(strcmp(RxBuf[0],"request_temp_data") == 0)
-			{
-				printf("temp func received \n");
-				/*call temp_read()*/
-			}
-
-			else if(strcmp(RxBuf[0],"request_light_val") == 0)
-			{
-				printf("Light val func received \n");
-				/*call light_val()*/
-			}
-
-			else if(strcmp(RxBuf[0],"request_sys_state") == 0)
-			{
-				printf("System State func received \n");
-				/*System State func()*/
-			}
-
-			else if(strcmp(RxBuf[0],"close") == 0)
-			{
-				printf("Closing Socket \n");
-				socket_flag = 0;
-				close(socket_fd);
-				//return 0;
-			}
-				
-			else 
-				printf("Unrecognized command. Please try again!\n");
-		}
-	}
-}
