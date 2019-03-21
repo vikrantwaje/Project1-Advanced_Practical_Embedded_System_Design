58aa2ee1c97e6657f0ef406782f85a9ffd48fca3
TanmayChaturvedi1 <tanmay.chaturvedi@colorado.edu>
2019-03-21 15:46:37 -0600

    server.c modified

--- src/server.c
+++ src/server.c
@@ -32,66 +32,62 @@ int server_establish()
 	int recv_stat;
 	char str[30];
 	int send_data_len;
-	
-	if( socket_setup() )
+
+
+	int socket_flag = 0;	/*Not Connected*/
+
+	while( socket_flag == 0 )
 	{
-
-		int socket_flag = 0;	/*Not Connected*/
-
-		while( socket_flag == 0 )
+		if( socket_connect() )
 		{
-			if( socket_connect() )
+			socket_flag = 1;
+
+			while(socket_flag == 1)
 			{
-				socket_flag = 1;
-
-				while(socket_flag == 1)
+				memset(str,0,sizeof(str));
+				if((recv_stat = recv(sock_stat, str,sizeof(str),0) < 0))
 				{
-					memset(str,0,sizeof(str));
-					if((recv_stat = recv(sock_stat, str,sizeof(str),0) < 0))
-					{
-						perror("read error");
-						break;
-					}
-
-					RxBuf[0] = str;
-
-					if(strcmp(RxBuf[0],"request_temp_data") == 0)
-					{
-						printf("temp func received \n");
-						/*call temp_read()*/
-					}
-
-					else if(strcmp(RxBuf[0],"request_light_val") == 0)
-					{
-						printf("Light val func received \n");
-						/*call light_val()*/
-					}
-
-					else if(strcmp(RxBuf[0],"request_sys_state") == 0)
-					{
-						printf("System State func received \n");
-						/*System State func()*/
-					}
-
-					else if(strcmp(RxBuf[0],"close") == 0)
-					{
-						printf("Closing Socket \n");
-						socket_flag = 0;
-						close(socket_fd);
-						break;
-					}
-
-					else 
-						printf("Unrecognized command. Please try again!\n");
+					perror("read error");
+					break;
 				}
-			}
-			else return 0;
-		}
 
+				RxBuf[0] = str;
+
+				if(strcmp(RxBuf[0],"request_temp_data") == 0)
+				{
+					printf("temp func received \n");
+					/*call temp_read()*/
+				}
+
+				else if(strcmp(RxBuf[0],"request_light_val") == 0)
+				{
+					printf("Light val func received \n");
+					/*call light_val()*/
+				}
+
+				else if(strcmp(RxBuf[0],"request_sys_state") == 0)
+				{
+					printf("System State func received \n");
+					/*System State func()*/
+				}
+
+				else if(strcmp(RxBuf[0],"close") == 0)
+				{
+					printf("Closing Socket \n");
+					socket_flag = 0;
+					close(socket_fd);
+					break;
+				}
+
+				else 
+					printf("Unrecognized command. Please try again!\n");
+			}
+		}
+		else return 0;
 	}
-	else 
-		return 0;
+
 }
+
 
 
 
@@ -105,6 +101,22 @@ int server_establish()
  * @return 0 if error, 1 if success
  ********************************************************************************************/
 int socket_setup()
+{
+
+}
+
+
+/*******************************************************************************************
+ * @brief Socket Listening Routines
+ *
+ * Call the listen() and accept() and returns 1 on successful connection
+ * with an external client
+ 
+ * @param null
+ *
+ * @return 0 if error, 1 if success
+ ********************************************************************************************/
+int socket_connect(void)
 {
 		socket_fd = socket(AF_INET, SOCK_STREAM,0);
 
@@ -124,21 +136,6 @@ int socket_setup()
 			perror("Server: bind()");
 			return 0;
 		}
-}
-
-
-/*******************************************************************************************
- * @brief Socket Listening Routines
- *
- * Call the listen() and accept() and returns 1 on successful connection
- * with an external client
- 
- * @param null
- *
- * @return 0 if error, 1 if success
- ********************************************************************************************/
-int socket_connect(void)
-{
 
 		listen(socket_fd, 5);
 
